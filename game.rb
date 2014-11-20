fc7e783b1e6bbfeb8ac0179bd744cb34e374a460
Sergio Barrera <sdbeng@gmail.com>
2014-11-20 10:25:37 -0800

    Add initialize and add_player methods

--- game.rb
+++ game.rb
@@ -1,2 +1,18 @@
-class Player
+class Game
+
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_player(a_player)
+		@players.push(a_player)
+	end
+
+
 end
+
+#test run block
+knuckleheads = Game.new("Knuckleheads")
+puts knuckleheads.title
+#knuckleheads.add_player(player1)
